020e6444030e52b140293e2a1bf4fdef918997b8
RiteshYadav1926 <ritesh192624@gmail.com>
2026-06-29 18:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -183,10 +183,3 @@ function mobileMenu() {
         });
 
     });
-
-}const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
